9e613b33c21f94450031d883dc534f5c594ad6d0
przemo246 <przemo246@gmail.com>
2022-12-09 18:23:20 +0100

    chore: remove unnecessary code, update import

--- frontend_client/src/index.tsx
+++ frontend_client/src/index.tsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { DAppProvider } from "@usedapp/core";
-import { useDappConfig } from "./config";
+import { useDappConfig } from "./useDappConfig";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 
@@ -33,8 +32,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
