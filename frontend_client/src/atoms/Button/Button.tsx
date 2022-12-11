9632b3b64693df7aca3a5f3b2a14837d2e50f6b4
przemo246 <przemo246@gmail.com>
2022-12-11 22:02:35 +0100

    feat: add component

---
+++ frontend_client/src/atoms/Button/Button.tsx
@@ -0,0 +1,34 @@
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+  variant?: "primary" | "secondaryFull" | "secondaryGhost" | "tertiary";
+  className?: string;
+}
+
+const variants = {
+  primary:
+    "text-white shadow-[5px_5px_0px_#F55D3E] hover:shadow-[2px_2px_0px_#F55D3E] transition-shadow bg-neutral-900",
+  secondaryFull:
+    "transition text-white border border-red-600 bg-red-600 hover:bg-[#F44B2A] hover:border-[#F44B2A]",
+  secondaryGhost:
+    "transition text-red-600 border border-red-600 bg-transparent hover:bg-red-600 hover:text-white",
+  tertiary:
+    "transition bg-indigo-600 text-white px-6 py-2 hover:bg-indigo-700 text-sm"
+};
+
+export const Button: React.FunctionComponent<ButtonProps> = ({
+  onClick,
+  children,
+  variant = "primary",
+  className = ""
+}) => {
+  return (
+    <button
+      type="button"
+      className={`rounded-[30px] px-10 py-2.5 text-sm font-medium ${variants[variant]} ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
