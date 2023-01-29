28c36982340997a1bb1bf072315aecdf6433cfe1
przemo246 <przemo246@gmail.com>
2023-01-29 20:26:50 +0100

    chore: use alchemy api key or fallback to default

--- frontend_client/src/useDappConfig.ts
+++ frontend_client/src/useDappConfig.ts
@@ -1,12 +1,14 @@
 import { Config, OptimismGoerli } from "@usedapp/core";
 import { getDefaultProvider } from "ethers";
 
+const ALCHEMY_API_KEY = process.env.REACT_APP_ALCHEMY_API_KEY;
+
 export const useDappConfig: Config = {
   readOnlyUrls: {
     [OptimismGoerli.chainId]: getDefaultProvider(
-      `https://opt-goerli.g.alchemy.com/v2/${
-        process.env.ALCHEMY_API_KEY ?? "https://goerli.optimism.io"
-      }`
+      ALCHEMY_API_KEY
+        ? `https://opt-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`
+        : "https://goerli.optimism.io"
     )
   }
 };
